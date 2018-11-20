6e4b5169dbbbd88b6024a360c4921ca923cdfe55
Ignjatovic David <Ignjatovicdavid1@gmail.com>
2018-11-20 11:19:45 +0100

    First Step

---
+++ car.cpp
@@ -0,0 +1,20 @@
+#include "car.h"
+
+enum CarType get_type(Car car1){
+  return AIXAM;
+}
+enum Color get_color(Car car1){
+  return GREEN;
+}
+double get_fill_level(Car car1){
+  return 0.0;
+}
+double get_acceleration_rate(Car car1){
+  return 0.0;
+}
+double get_speed(Car car1){
+  return 0.0;
+}
+void init(){
+
+}
--- car.h
+++ car.h
@@ -13,4 +13,19 @@
 #ifndef ___CAR_H
 #define ___CAR_H
 
+
+
+enum Color {RED, GREEN, BLUE, ORANGE, SILVER, BLACK };
+enum CarType {AIXAM,FIAT_MULTIPLA, JEEP };
+typedef struct CarImplementation* Car;
+
+Car get_car(Car car1);
+enum CarType get_type(Car car1);
+enum Color get_color(Car car1);
+double get_fill_level(Car car1);
+double get_acceleration_rate(Car car1);
+double get_speed(Car car1);
+void init();
+
+
 #endif
