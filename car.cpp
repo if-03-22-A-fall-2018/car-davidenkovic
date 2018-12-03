15566dd549fede4094736000aa659fe2ac6db1f4
Ignjatovic David <if170055@WS8A19.local>
2018-12-03 13:34:11 +0100

    übung

--- car.cpp
+++ car.cpp
@@ -1,27 +1,60 @@
 #include "car.h"
+#define CARCOUNT 6
+
+struct CarImplementation{
+  enum CarType carType;
+  enum Color color;
+  double fillLevel;
+  double accelerationRate;
+  int speed;
+  bool isrented;
+};
+
+static struct CarImplementation aixam = {AIXAM,RED,16.0,0.0,0,false};
+static struct CarImplementation fiat = {FIAT_MULTIPLA,GREEN,65.0,0.0,0,false};
+static struct CarImplementation fiat2 = {FIAT_MULTIPLA,BLUE,65.0,0.0,0,false};
+static struct CarImplementation fiat3 = {FIAT_MULTIPLA,ORANGE,65.0,0.0,0,false};
+static struct CarImplementation jeep = {JEEP,SILVER,80.0,0.0,0,false};
+static struct CarImplementation jeep2 = {JEEP,BLACK,80.0,0.0,0,false};
 
 
-CarType get_type(CarType car1){
-  return AIXAM;
+Car carpark[CARCOUNT] = {&aixam,&fiat,&fiat2,&fiat3,&jeep,&jeep2};
+
+
+Car get_car(CarType car){
+  for (int i = 0; i < CARCOUNT; i++) {
+    if (car == carpark[i]->carType && carpark[i]->isrented == false) {
+      carpark[i]->isrented = true;
+      return carpark[i];
+    }
+  }
+  return 0;
 }
-Color get_color(Car car1){
-  return GREEN;
+enum CarType get_type(Car car){
+  return car->carType;
 }
-double get_fill_level(Car car1){
-  return 0.0;
+enum Color get_color(Car car){
+  return car->color;
 }
-double get_acceleration_rate(Car car1){
-  return 0.0;
+double get_fill_level(Car car){
+  return car->fillLevel;
 }
-double get_speed(Car car1){
-  return 0.0;
+double get_acceleration_rate(Car car){
+ return car->accelerationRate;
+}
+int get_speed(Car car){
+  return car->speed;
 }
 void init(){
+  for (int i = 0; i < CARCOUNT; i++) {
+    carpark[i]->isrented = false;
+    carpark[i]->accelerationRate = 0;
+  }
+}
+void accelerate(Car car){
 
 }
-void accelerate(Car car1){
-
-}
-double set_acceleration_rate(Car car1, double acceleration){
-  return 0.0;
+double set_acceleration_rate(Car car, double acceleration){
+   car->accelerationRate = acceleration;
+   return car->accelerationRate;
 }
--- car.h
+++ car.h
@@ -17,17 +17,19 @@
 
 enum Color {RED, GREEN, BLUE, ORANGE, SILVER, BLACK };
 enum CarType {AIXAM,FIAT_MULTIPLA, JEEP };
+
 typedef struct CarImplementation* Car;
 
-Car get_car(CarType car1);
-CarType get_type(Car car1);
-Color get_color(Car car1);
-double get_fill_level(Car car1);
-double get_acceleration_rate(Car car1);
-double get_speed(Car car1);
+
+Car get_car(CarType car);
+enum CarType get_type(Car car);
+enum Color get_color(Car car);
+double get_fill_level(Car car);
+double get_acceleration_rate(Car car);
+int get_speed(Car car);
 void init();
-void accelerate(Car car1);
-double set_acceleration_rate(Car car1, double acceleration);
+void accelerate(Car car);
+double set_acceleration_rate(Car car, double acceleration);
 
 
 #endif
